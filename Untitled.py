80344299bd2bdaafa1e1cc1a970a7d58451bef6f
akshaywararkar <akshaywararkar23@gmail.com>
2022-11-16 15:08:38 +0530

    added models list

---
+++ Untitled.py
@@ -0,0 +1,74 @@
+if Model == "Anything-V3.0 fp16 [38c1ebe3] (torrent)":
+if Model == "Anything-V3.0 fp32 [1a7df6b8] (torrent)":
+if Model == "Anything-V3.0 Full EMA [6569e224] (torrent)":
+if Model == "Cafe Unofficial Instagram TEST Model [50b987ae] (torrent)":
+if Model == "SmirkingFace SF_EB_1.0_ema_vae.ckpt [7f115f17] (direct)":
+if Model == "SmirkingFace SF_EB_1.0_ema_vae.ckpt [7f115f17] (torrent)":
+if Model == "SmirkingFace SF_EB_1.0_noema_vae.ckpt [7010a578] (direct)":
+if Model == "SmirkingFace SF_EB_1.0_noema_vae.ckpt [7010a578] (torrent)":
+if Model == "SXD-v0.8 (generate)":
+if Model == "gg1342_testrun1_pruned.ckpt [43076286] [2ccc3e58] (torrent)":
+if Model == "Pyro's Blowjob Model v1.0 [9b5251e8] (direct)":
+if Model == "Pyro's Blowjob Model v1.0 [9b5251e8] (mega)":
+if Model == "Pyro's POV Cowgirl Model Model A (mega)":
+if Model == "Pyro's POV Cowgirl Model Model B (mega)":
+if Model == "Easter easter_e5 [da453598] (mega)":
+if Model == "Easter easter_e5 [da453598] (torrent)":
+if Model == "Easter easter_e4 [3997b596] (mega)":
+if Model == "Easter easter_e3 [9c5ade34] (mega)":
+if Model == "Easter easter_e3 [9c5ade34] (torrent)":
+if Model == "Hentai Diffusion HD-16.ckpt [e2ec4647] (direct)":
+if Model == "Hentai Diffusion RD1412.ckpt [2140af02] [5b87f0e6] [4fdde306] (direct)":
+if Model == "Hentai Diffusion RD1212.ckpt [a1f5a67e] [37b5398c] [3b3459c8] (direct)":
+if Model == "Bare Feet / Full Body b4_t16_noadd [03e33bb4] [2bd8166c] [9012c514] (torrent)":
+if Model == "Lewd Diffusion 70k 2e [f4030c43] (torrent)":
+if Model == "Lewd Diffusion 70k 1e [950d323b] (torrent)":
+if Model == "Lewd Diffusion v0 [07734b46] (torrent)":
+if Model == "Yiffy yiffy-e18.ckpt [50ad914b] (direct)":
+if Model == "Yiffy yiffy-e18.ckpt [50ad914b] (torrent)":
+if Model == "Yiffy yiffy-e15.ckpt [4bb305c0] (direct)":
+if Model == "Yiffy yiffy-e15.ckpt [4bb305c0] (generate)":
+if Model == "Yiffy yiffy-e15.ckpt [4bb305c0] (direct)":
+if Model == "Yiffy yiffy-e15.ckpt [4bb305c0] (torrent)":
+if Model == "Yiffy yiffy-e13.ckpt [778b38ae] (direct)":
+if Model == "Yiffy yiffy-e13.ckpt [778b38ae] (torrent)":
+if Model == "SnackBar-General-V1-E11 [16d5a5c7] (generate)":
+if Model == "SnackBar-General-V1-E11 [16d5a5c7] (torrent)":
+if Model == "Furry Furry_epoch4.ckpt [323f8dd8] (torrent)":
+if Model == "Furry Furry_epoch4.ckpt [323f8dd8] (generate)":
+if Model == "Furry Furry_epoch4.ckpt [323f8dd8] (direct)":
+if Model == "Furry Furry_epoch1.ckpt [0c891127] (torrent)":
+if Model == "Furry Furry_epoch1.ckpt [0c891127] (direct)":
+if Model == "Furry Furry_epoch0.ckpt [8c19ee5a] (direct)":
+if Model == "Zack3D_Kinky-v1.ckpt [1a75d5c6] (generate)":
+if Model == "Zack3D_Kinky-v1.ckpt [1a75d5c6] (torrent)":
+if Model == "Anal Vore AVHumanFurryPony7.ckpt [68e2e32d] (mega)":
+if Model == "R34 r34_e4.ckpt [6e45cf2c] (torrent)":
+if Model == "R34 r34_e4.ckpt [6e45cf2c] (mega)":
+if Model == "R34 r34_e3.ckpt [57ea6f43] (mega)":
+if Model == "R34 r34_e2.ckpt [778b68b1] (torrent)":
+if Model == "R34 r34_e2.ckpt [778b68b1] (mega)":
+if Model == "R34 r34_e1.ckpt [f9000e4e] (torrent)":
+if Model == "R34 r34_e1.ckpt [f9000e4e] (mega)":
+if Model == "R34 r34_150k_epoch0-pruned-fp16.ckpt [7dc34283] (torrent)":
+if Model == "R34 r34_150k_epoch0.ckpt [7c3721c3] (torrent)":
+if Model == "R34 r34_150k_epoch0.ckpt [7c3721c3] (mega)":
+if Model == "Gape gape60 [25396b85] (torrent)":
+if Model == "Gape gape60 [25396b85] (mega)":
+if Model == "Gape gape22 [d69e1a23] (torrent)":
+if Model == "Gape gape22 [d69e1a23] (mega)":
+if Model == "Gape gape18 (torrent)":
+if Model == "sd-wikiart-v2.ckpt. [8f32b8df] (torrent)":
+if Model == "Pachu-Diffusion [54d5d199] (direct)":
+if Model == "TestFurry_5.ckpt [b1f1855e] (torrent)":
+if Model == "cookie_sd_pony_run_a12 float16 [2ce7dcf5] [67ff5385] (direct)":
+if Model == "cookie_sd_pony_run_a12 float16 [2ce7dcf5] [67ff5385] (torrent)":
+if Model == "cookie_sd_pony_run_a12 float32 [2ce7dcf5] [67ff5385] (direct)":
+if Model == "cookie_sd_pony_run_a12 float32 [2ce7dcf5] [67ff5385] (mega)":
+if Model == "pony-diffusion pony-diffusion-v2 [5bdc40aa] (mega)":
+if Model == "pony-diffusion pony-diffusion-v1 [9070b574] (mega)":
+if Model == "mio-wd-v1.2-e24-ex-ad [125f9ece] (torrent)":
+if Model == "mio-wd-v1.2-e24-ex-ad [125f9ece] (direct)":
+if Model == "fubuki-ld-v1-e13-ex-ad [2af6d20f] (torrent)":
+if Model == "asuka-ld-v1-e4-ex-ad [87074080] (torrent)":
+if Model == "tomoko-kuroki-ld-v1-e20-ex-ad [6095e7ab] (torrent)":
