be0b5d4948a5d243d4202e71b11141bcd6e146d9
akshaywararkar <akshaywararkar23@gmail.com>
2022-12-28 19:03:57 +0530

    added old invoke nb

---
+++ invokeai_old/invoke.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eu
+
+# ensure we're in the correct folder in case user's CWD is somewhere else
+scriptdir=$(dirname "$0")
+cd "$scriptdir"
+
+export INVOKEAI_ROOT="$scriptdir"
+
+# set required env var for torch on mac MPS
+ if [ "$(uname -s)" == "Darwin" ]; then
+     export PYTORCH_ENABLE_MPS_FALLBACK=1
+ fi
+
+if [ "$0" != "bash" ]; then
+    python /usr/local/bin/invoke.py --web
+else # in developer console
+    python --version
+    echo "Press ^D to exit"
+    export PS1="(InvokeAI) \u@\h \w> "
+fi
